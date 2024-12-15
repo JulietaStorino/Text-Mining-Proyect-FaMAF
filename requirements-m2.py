bec42fceb50e39620ebc5a2e0880e715d8322366
JulietaStorino <julieta_storino@mi.unc.edu.ar>
2024-12-14 23:03:05 -0300

    modificación de los requisitos

---
+++ requirements-m2.py
@@ -0,0 +1,8 @@
+import nltk
+
+# Descarga los recursos necesarios para el correcto funcionamiento de la librería nltk
+nltk.download('cess_esp')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('averaged_perceptron_tagger_eng')
+nltk.download('punkt')
+nltk.download('punkt_tab')
